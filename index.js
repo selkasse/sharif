0de33d7e3f47c0f3768575fc1c689457854910e5
Sharif Elkassed <selkasse@gmu.edu>
2022-05-28 16:49:18 -0400

    📦 NEW: check node version

--- index.js
+++ index.js
@@ -4,13 +4,11 @@ const figlet = require("figlet");
 const welcome = require("cli-welcome");
 const pkgJSON = require("./package.json");
 const alert = require("sharif-cli-alerts");
+const checkNode = require("cli-check-node");
 
-//* Alerts
-const symbol = require("log-symbols");
-const success = chalk.green.inverse;
-const warning = chalk.keyword("orange").inverse;
-const info = chalk.blue.inverse;
-const error = chalk.red.bold.inverse;
+//* Specify a minimum major version of node
+//? Should set this to the Maintenance LTS version
+checkNode("14");
 
 welcome({
   title: `Sharif Elkassed`,
@@ -26,7 +24,6 @@ console.log(chalk.cyanBright.bold(figlet.textSync("sharif")));
 
 console.log(chalk.blueBright.bold(figlet.textSync("   elkassed")));
 
-// I'm ${chalk.keyword("blue").bgCyan.bold(" Sharif Elkassed ")} :)
 console.log(`
 ${chalk.italic(`I like bridging ideas and systems together to further my skillset.
 
@@ -46,29 +43,3 @@ I'm always looking for that next challenge.`)}
 `);
 
 alert({ type: `info`, msg: `https://skyshell.tech`, name: ` COMING SOON ` });
-//* To make the file executable:
-//? From the terminal, run:
-//* chmod +x index.js
-
-//* From there, we are able to execute the file by running:
-//? ./index.js
-//* BUT, the terminal doesn't know to run the file with node
-//? In other words, it is trying to read it is a shell script
-
-//* To get around that, we need to tell the terminal to use Node.js
-//* We do that by putting a shebang at the top of the file
-//? #! /usr/bin/env node <-- this is a shebang
-
-//* We then create the package.json file manually, like so:
-// {
-//   "name": "sharif",      //* the name of the package on the npm registry
-//   "version": "1.0.0",
-//   "bin": {
-//     "sharif": "index.js" //* the name of the command to run
-//   }
-// }
-
-//* After creating the package.json, we can then run:
-//? npm link
-//* Don't worry too much about the details;
-//? Just know that it makes developing npm packages easier
