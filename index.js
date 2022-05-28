dbfda5c0e4f16aacc2fa447cb61203bb227507f5
Sharif Elkassed <selkasse@gmu.edu>
2022-05-28 18:18:23 -0400

    📖 DOC: test

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const linkedIn = chalk.hex("#00a0dc").bold.inverse;
 const website = chalk.hex("#A357EBf").bold.inverse;
 const dim = chalk.dim;
 
-//test commen
+//test comme
 
 //* Run the code in an IIFE so it does not pollute the global namespace
 (() => {
