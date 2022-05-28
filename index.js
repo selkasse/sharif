cabd1188fcbeb260a69597a94a4179068f5d8a25
Sharif Elkassed <selkasse@gmu.edu>
2022-05-27 22:19:11 -0400

    📦 NEW: brighter colors

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ I like bridging systems and ideas together to further my skillset.
 I'm always looking for that next challenge.
 
 
-🧙  ${chalk.green("GitHub:")}   https://github.com/selkasse
-⏩  ${chalk.blue("LinkedIn:")} https://linkedin.com/in/selkassed
-🌍  ${chalk.magenta("Website:")}  https://saleshorse.org
+🧙  ${chalk.cyan("GitHub:")}   https://github.com/selkasse
+⏩  ${chalk.blueBright("LinkedIn:")} https://linkedin.com/in/selkassed
+🌍  ${chalk.magentaBright("Website:")}  https://saleshorse.org
 
 `);
 //* To make the file executable:
