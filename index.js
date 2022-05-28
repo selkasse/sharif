73fb68d9596fd8e4b9a074dbdaf196c9c9c33873
Sharif Elkassed <selkasse@gmu.edu>
2022-05-28 18:22:34 -0400

    👌 IMPROVE: remove test comment after fixing local git helper homebrew issue

--- index.js
+++ index.js
@@ -15,8 +15,6 @@ const linkedIn = chalk.hex("#00a0dc").bold.inverse;
 const website = chalk.hex("#A357EBf").bold.inverse;
 const dim = chalk.dim;
 
-//test comm
-
 //* Run the code in an IIFE so it does not pollute the global namespace
 (() => {
   init();
