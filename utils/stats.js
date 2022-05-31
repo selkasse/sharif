f9961f467e48274f7585314809b7a9f248a8b373
Sharif Elkassed <selkasse@gmu.edu>
2022-05-31 01:33:08 -0400

    👌 IMPROVE: clean up

--- utils/stats.js
+++ utils/stats.js
@@ -1,18 +1,22 @@
 import fetch from 'node-fetch'
+import ora from 'ora'
 import handleError from 'cli-handle-error'
 
 const API_URL = `https://api.github.com/users/selkasse`
+const spinner = ora({ text: `` })
 
 export default async () => {
+  spinner.start(`Fetching GitHub followers...`)
   try {
     const res = await fetch(API_URL)
     const data = await res.json()
-
     const ghFollowers = data.followers
+    spinner.stop()
     console.log(
       `I have ${ghFollowers} followers on GitHub. Care to add another? 😃\n`
     )
   } catch (error) {
+    spinner.stop()
     handleError(`Unable to fetch GitHub data at this time`, error, false, false)
   }
 }
